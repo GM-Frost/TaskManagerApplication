7be5178c6d2be2f7da423a95c8646b674da78c83
Frosty <nayanbastola777@gmail.com>
2023-08-18 10:09:46 -0400

    Dependencies Setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import "./global.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Register from "./pages/Register.tsx";
+import Login from "./pages/Login.tsx";
+import Home from "./pages/Home.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/Home",
+    element: <Home />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
